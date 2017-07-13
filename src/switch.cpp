34ec36266158e8d9c3f7737fd5fc9f183000b51a
TAR <texasaerialrobotics@gmail.com>
2017-07-13 18:22:58 -0500

    Added package to repo

---
+++ src/switch.cpp
@@ -0,0 +1,84 @@
+/**
+ * @file offb_node.cpp
+ * @brief offboard example node, written with mavros version 0.14.2, px4 flight
+ * stack and tested in Gazebo SITL
+ */
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
+mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 1, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
+
+    // wait for FCU connection
+    while(ros::ok() && current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 2;
+
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "LAND";  
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+
+    while(ros::ok()){
+	std::cout << current_state.mode << std::endl;
+        if( current_state.mode != "ALT_HOLD" && (ros::Time::now() - last_request > ros::Duration(5.0))){
+            last_request = ros::Time::now();
+	    ROS_INFO("k");
+        } else {
+		ROS_INFO("ALT");
+            	if( current_state.armed && (current_state.mode == "ALT_HOLD")){
+			ROS_INFO("ARMED");
+			if(set_mode_client.call(offb_set_mode)){
+				ROS_INFO("made it?");
+			} else {
+				ROS_INFO("FUCK");
+			}
+                }
+                last_request = ros::Time::now();
+            }
+        
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
