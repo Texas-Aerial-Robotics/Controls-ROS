075c6d71baff31139ea705d18b89e7dcca385b55
TAR <texasaerialrobotics@gmail.com>
2017-07-18 21:41:50 -0500

    takeoff?

--- src/takeoff.cpp
+++ src/takeoff.cpp
@@ -11,10 +11,17 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <mavros_msgs/AttitudeTarget.h>
+#include <sensor_msgs/Range.h>
 
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
+}
+
+sensor_msgs::Range rngfnd;
+void rng_cb(const sensor_msgs::Range::ConstPtr& msg){
+    rngfnd = *msg;
 }
 
 int main(int argc, char **argv)
@@ -23,16 +30,13 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
+            ("mavros/state", 1, state_cb);
+    ros::Subscriber rng_sub = nh.subscrube<sensor_msgs::Range>("mavros/rangefinder/rangefinder",1,rng_cb);
 
-    ros::Publisher att_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("mavros/setpoint_attitude/cmd_vel", 10);
-    ros::Publisher att_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_attitude/attitude", 10);
-    ros::Publisher att_thr_pub = nh.advertise<std_msgs::Float64>("mavros/setpoint_attitude/att_throttle", 10);
+    ros::Publisher att_pub = nh.advertise<mavros_msgs::AttitudeTarget>("mavros/setpoint_raw/attitude", 10);
 
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
@@ -43,37 +47,29 @@ int main(int argc, char **argv)
         rate.sleep();
     }
 
-    geometry_msgs::PoseStamped pose1;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 0;
-    pose.pose.orientation.x = 0;
-    pose.pose.orientation.y = 0;
-    pose.pose.orientation.z = 0;
-    pose.pose.orientation.w = 1;
-
-    geometry_msgs::TwistStamped twist;
-    twist.twist.linear.x = 0;
-    twist.twist.linear.y = 0;
-    twist.twist.linear.z = 0;
-    twist.twist.angular.x = 0;
-    twist.twist.angular.y = 0;
-    twist.twist.angular.z = 0;
-
-    std_msgs::Float64 thr;
-    thr.data = .63;
+    mavros_msgs::AttitudeTarget att;
+    att.orientation.x=0;
+    att.orientation.y=0;
+    att.orientation.z=0;
+    att.orientation.w=0;
+    att.body_rate.x=0;
+    att.body_rate.y=0;
+    att.body_rate.z=0;
+    att.thrust=.63;
+    
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
-	att_vel_pub.publish(twist);
-        att_pos_pub.publish(pose1);
-        att_thr_pub.publish(thr);
+	att_pub.publish(att);
         ros::spinOnce();
         rate.sleep();
     }
 
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "GUIDED_NOGPS";
+
+    mavros_msgs::SetMode alt_set_mode;
+    alt_set_mode.request.custom_mode = "LAND";
 
     mavros_msgs::CommandBool arm_cmd;
     arm_cmd.request.value = true;
@@ -99,9 +95,13 @@ int main(int argc, char **argv)
             }
         }
 
-	att_vel_pub.publish(twist);
-        att_pos_pub.publish(pose1);
-	att_thr_pub.publish(thr);
+	att_pub.publish(att);
+ 
+        if(rngfnd.range>1.5){
+            if(set_mode_client.call(alt_set_mode) && alt_set_mode.response.success){
+                ROS_INFO("land");
+            }
+        }
 
         ros::spinOnce();
         rate.sleep();
