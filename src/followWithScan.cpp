4c39f4e58ea8f70778b542f32268e4caa6990e92
Umer Salman <umer936@hotmail.com>
2018-07-07 20:24:32 -0500

    Cleanup cleanup everybody everywhere

--- src/followWithScan.cpp
+++ src/followWithScan.cpp
@@ -44,8 +44,8 @@ void state_cb(const mavros_msgs::State::ConstPtr& msg)
 //get current position of drone
 void pose_cb(const nav_msgs::Odometry::ConstPtr& msg)
 {
-	current_pose = *msg;
-	// ROS_INFO("x: %f y: %f z: %f", current_pose.pose.pose.position.x, current_pose.pose.pose.position.y, current_pose.pose.pose.position.z);
+  current_pose = *msg;
+  // ROS_INFO("x: %f y: %f z: %f", current_pose.pose.pose.position.x, current_pose.pose.pose.position.y, current_pose.pose.pose.position.z);
 }
 void mode_cb(const std_msgs::String::ConstPtr& msg)
 {
@@ -80,7 +80,6 @@ void setHeading(float heading)
   waypoint.pose.orientation.x = qx;
   waypoint.pose.orientation.y = qy;
   waypoint.pose.orientation.z = qz;
-
 }
 void setHeading_cb(const std_msgs::Float64::ConstPtr& msg)
 {
@@ -121,14 +120,14 @@ void waypoint_update(const geometry_msgs::PoseStamped::ConstPtr& msg)
   float way_y = waypoint_eval.pose.position.y;
   float way_z = waypoint_eval.pose.position.z;
 
-  if (!currentlyAvoiding)
+  if(!currentlyAvoiding)
   {
-    if (way_z < z_max && way_z > z_min && way_x < x_max && way_x > x_min && way_y < y_max && way_y > y_min)
+    if(way_z < z_max && way_z > z_min && way_x < x_max && way_x > x_min && way_y < y_max && way_y > y_min)
     {
-          waypoint.pose.position.x = way_x;
-          waypoint.pose.position.y = way_y;
-          waypoint.pose.position.z = way_z;
-          setDestination(way_x,way_y,way_z);
+      waypoint.pose.position.x = way_x;
+      waypoint.pose.position.y = way_y;
+      waypoint.pose.position.z = way_z;
+      setDestination(way_x,way_y,way_z);
     }
     else
     {
@@ -154,18 +153,19 @@ int main(int argc, char** argv)
   ros::param::get("/contols_params/safety_limits/xlim/max", z_max);
 
   ros::Rate rate(5.0);
-  ros::Subscriber state_sub = controlnode.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
-  ros::Publisher set_vel_pub = controlnode.advertise<mavros_msgs::PositionTarget>("mavros/setpoint_raw/local", 10);
-  ros::Publisher local_pos_pub = controlnode.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
   ros::Publisher gym_offset_pub = controlnode.advertise<std_msgs::Float64>("gymOffset", 1);
+  ros::Publisher local_pos_pub = controlnode.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
   ros::Publisher run_start_pub = controlnode.advertise<std_msgs::Float64>("runStartTime", 1);
-  ros::Subscriber currentPos = controlnode.subscribe<nav_msgs::Odometry>("mavros/global_position/local", 10, pose_cb);
-  ros::Subscriber waypointSubscrib = controlnode.subscribe<geometry_msgs::PoseStamped>("waypoint", 10, waypoint_update);
-  ros::Subscriber mode_sub = controlnode.subscribe<std_msgs::String>("mode", 10, mode_cb);
   ros::Subscriber collision_sub = controlnode.subscribe<sensor_msgs::LaserScan>("spur/laser/scan", 1, scan_cb);
-  ros::ServiceClient arming_client = controlnode.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
-  ros::ServiceClient takeoff_client = controlnode.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
+  ros::Subscriber currentPos = controlnode.subscribe<nav_msgs::Odometry>("mavros/global_position/local", 10, pose_cb);
   ros::Subscriber heading_pub = controlnode.subscribe<std_msgs::Float64>("setHeading", 1, setHeading_cb);
+  ros::Subscriber mode_sub = controlnode.subscribe<std_msgs::String>("mode", 10, mode_cb);
+  ros::Subscriber state_sub = controlnode.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+  ros::Subscriber waypointSubscrib = controlnode.subscribe<geometry_msgs::PoseStamped>("waypoint", 10, waypoint_update);
+  ros::ServiceClient arming_client = controlnode.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+  ros::ServiceClient land_client = controlnode.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+  ros::ServiceClient set_mode_client = controlnode.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+  ros::ServiceClient takeoff_client = controlnode.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
 
   // wait for FCU connection
   while (ros::ok() && !current_state.connected)
@@ -182,6 +182,8 @@ int main(int argc, char** argv)
 
     RUN_START_TIME = ros::Time::now().toSec();
   }
+
+  // Publish mission start time
   std_msgs::Float64 runStartTime;
   runStartTime.data = RUN_START_TIME;
   run_start_pub.publish(runStartTime);
@@ -233,21 +235,24 @@ int main(int argc, char** argv)
   }
   ROS_INFO("Takeoff initialized");
   sleep(10);
+
   setDestination(0,0,1.5);
-  //move foreward
   setHeading(0);
+
+  // 2D spinning LIDAR params
   float tollorance = .35;
   float scanMinRange = .45;
   float scanMaxRange = 1.75;
+
   while(ros::ok())
   {
     ros::spinOnce();
-
     rate.sleep();
 
+    // 2D LIDAR obstacle avoidance
     int scanRayIndex = 2;
-	  while(((scanRayIndex+2) < 1024)) {
-  	  scanRayIndex++;
+    while(((scanRayIndex+2) < 1024)) {
+      scanRayIndex++;
       ros::spinOnce();
 
       // check if 5 consecutive readings are in bounds we care about
@@ -257,22 +262,23 @@ int main(int argc, char** argv)
           ((current_2D_scan.ranges[scanRayIndex+1] < scanMaxRange) && (current_2D_scan.ranges[scanRayIndex+1] > scanMinRange)) &&
           ((current_2D_scan.ranges[scanRayIndex+2] < scanMaxRange) && (current_2D_scan.ranges[scanRayIndex+2] > scanMinRange))))
       {
-        break;
+        break; // if so, stop scanning and move on
       }
+      // Prints if we got a few detections but not a full 5
       if(current_2D_scan.ranges[scanRayIndex] < scanMaxRange && current_2D_scan.ranges[scanRayIndex] > scanMinRange)
       {
-        	ROS_INFO("Index[%d]: %f", scanRayIndex, current_2D_scan.ranges[scanRayIndex]);
+        ROS_INFO("Index[%d]: %f", scanRayIndex, current_2D_scan.ranges[scanRayIndex]);
       }
-      if ((scanRayIndex+3) == 1024) // scanRayIndex+2 because checking multiple rays and +1 because less than in while loop
+      if((scanRayIndex+3) == 1024) // scanRayIndex+2 because checking multiple rays and +1 because less than in while loop
       {
         currentlyAvoiding = false;
       }
-	  }
-	  if (scanRayIndex < 1023 && scanRayIndex > 0 && current_2D_scan.ranges[scanRayIndex] < scanMaxRange && current_2D_scan.ranges[scanRayIndex] > scanMinRange)
-	  {
-  		double angle_of_obstacle_RAD = current_2D_scan.angle_increment*scanRayIndex;
-  		ROS_INFO("Obstacle sighted @%f (current_2D_scan.angle_increment: %f * scanRayIndex: %d)", angle_of_obstacle_RAD, current_2D_scan.angle_increment, scanRayIndex);
-  		ROS_INFO("SKRTT SKRTT");
+    }
+    if(scanRayIndex < 1023 && scanRayIndex > 0 && current_2D_scan.ranges[scanRayIndex] < scanMaxRange && current_2D_scan.ranges[scanRayIndex] > scanMinRange)
+    {
+      double angle_of_obstacle_RAD = current_2D_scan.angle_increment*scanRayIndex;
+      ROS_INFO("Obstacle sighted @%f (current_2D_scan.angle_increment: %f * scanRayIndex: %d)", angle_of_obstacle_RAD, current_2D_scan.angle_increment, scanRayIndex);
+      ROS_INFO("-------------------SKRTT SKRTT-------------------");
 
       //get current position in gym frame
       float deg2rad = (M_PI/180);
@@ -281,13 +287,13 @@ int main(int argc, char** argv)
       float Z = 1.5;
       ROS_INFO("x: %f y: %f z: %f", X, Y, Z);
 
-  		double radius = current_2D_scan.ranges[scanRayIndex];
-  		double why = radius * sin(angle_of_obstacle_RAD);
-  		double ex = radius * cos(angle_of_obstacle_RAD);
+      double radius = current_2D_scan.ranges[scanRayIndex];
+      double why = radius * sin(angle_of_obstacle_RAD);
+      double ex = radius * cos(angle_of_obstacle_RAD);
 
-  		ROS_INFO("radius: %f, ex: %f, why: %f", radius, ex, why);
+      ROS_INFO("radius: %f, ex: %f, why: %f", radius, ex, why);
 
-  		setDestination((X + (ex/radius)*(-1.328)), (Y + (why/radius)*(-1.328)), Z);
+      setDestination((X + (ex/radius)*(-1.328)), (Y + (why/radius)*(-1.328)), Z);
       currentlyAvoiding = true;
 
       rate.sleep();
@@ -295,19 +301,16 @@ int main(int argc, char** argv)
       rate.sleep();
     }
 
-    if (!currentlyAvoiding)
+    if(!currentlyAvoiding)
     {
       if(MODE.data == "LAND")
       {
-        ros::ServiceClient land_client = controlnode.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
         mavros_msgs::CommandTOL srv_land;
-        if (land_client.call(srv_land) && srv_land.response.success)
+        if(land_client.call(srv_land) && srv_land.response.success)
           ROS_INFO("land sent %d", srv_land.response.success);
         else
         {
           ROS_ERROR("Landing failed");
-          ros::shutdown();
-          return -1;
         }
       }
       else if(MODE.data == "TAKEOFF")
@@ -317,12 +320,11 @@ int main(int argc, char** argv)
           rate.sleep();
           ros::spinOnce();
         }
-        sleep(8);
+        ros::Duration(8).sleep();
 
         while (current_state.mode != "GUIDED")
         {
           //set flight mode to guided
-          ros::ServiceClient set_mode_client = controlnode.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
           mavros_msgs::SetMode srv_setMode;
           srv_setMode.request.base_mode = 0;
           srv_setMode.request.custom_mode = "GUIDED";
@@ -338,11 +340,10 @@ int main(int argc, char** argv)
         // arming
         mavros_msgs::CommandBool arm_request;
         arm_request.request.value = true;
-        float armTime = ros::Time::now().toSec();
         while (!current_state.armed)
         {
-          ros::Duration(.1).sleep();
           arming_client.call(arm_request);
+          ros::Duration(.1).sleep();
           ros::spinOnce();
         }
         ROS_INFO("ARM sent %d", arm_request.response.success);
@@ -351,15 +352,14 @@ int main(int argc, char** argv)
 
         while (!takeoff_request.response.success)
         {
-          ros::Duration(.1).sleep();
           takeoff_client.call(takeoff_request);
+          ros::Duration(.1).sleep();
           ros::spinOnce();
         }
         ROS_INFO("Takeoff sent");
-        sleep(5);
+        ros::Duration(5).sleep();
       }
     }
-
 
     local_pos_pub.publish(waypoint);
     gym_offset_pub.publish(gymOffset);
